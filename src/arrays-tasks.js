ae5c2449b9ec3f4c693773ddbea30035195086cf
Denis <denis.shagim@icloud.com>
2024-11-28 09:14:55 +0300

    41 passing test

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -403,8 +403,10 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((count, value) => {
+    return count + (!value ? 1 : 0);
+  }, 0);
 }
 
 /**
